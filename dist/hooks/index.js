d568fd77456c41982a6ee59cf3b1e8a7195d9b4b
Trần Đức Tâm <kidnextdoor461993@gmail.com>
2025-05-10 10:10:54 +0700

    [Update] Zod version and optimize build.

--- dist/hooks/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.instanceOf = exports.arrayOf = void 0;
-var arrayOf_1 = require("./arrayOf");
-Object.defineProperty(exports, "arrayOf", { enumerable: true, get: function () { return arrayOf_1.arrayOf; } });
-var instanceOf_1 = require("./instanceOf");
-Object.defineProperty(exports, "instanceOf", { enumerable: true, get: function () { return instanceOf_1.instanceOf; } });
