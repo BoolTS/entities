e2e9373082435650c19e684452d8ced5cd6796db
Trần Đức Tâm <kidnextdoor461993@gmail.com>
2025-09-14 04:09:42 +0700

    [Update] Dependencies, move zod to peerDependencies, ..

---
+++ app.build.ts
@@ -0,0 +1,17 @@
+import { build } from "bun";
+
+(async () => {
+    const currentDir = import.meta.dir;
+
+    await build({
+        entrypoints: [`${currentDir}/src/index.ts`],
+        root: `${currentDir}/src`,
+        outdir: `${currentDir}/dist`,
+        publicPath: "../",
+        sourcemap: "external",
+        target: "node",
+        format: "esm",
+        packages: "external",
+        minify: true
+    });
+})();
